e534ce8fcb483c28bf8a197b2621a5144ab105a0
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-21 19:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function countdown(callback) {
 var doubler = 2
 var tripler = 3
 function createMultiplier(variable) {
-
+  mult = 1
+  if (variable === 2) {mult = 2}
+  if(variable === 3) {mult = 3}
+  return function(base) {
+    return base * variable
+  }
 }
