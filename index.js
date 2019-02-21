7118183809eba1240f814a8a3c9b69b63f15c006
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-21 19:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function createMultiplier(variable) {
 function multiplier(a,b) {
   return a * b
 }
-var doublerWithBind {multiplier.bind(null,2)}
-var triplerWithBind {multiplier.bind(null,3)}
+var doublerWithBind = multiplier.bind(null,2)
+var triplerWithBind = multiplier.bind(null,3)
